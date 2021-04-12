45fa01c1989d63fff346b7dc3010be3eb7213064
Shenjie Lin <linshenjie321@gmail.com>
2021-04-11 21:51:24 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		streamAPITrials();
+
+	}
+
+	private static void streamAPITrials() {
+
+		// creation of Stream using a literal array
+		Stream<String> streamOfStringFromLiteralArray = Stream.of("a", "b", "c", "d", "e", "f", "g");
+		// creation of Stream from a declared array
+		String[] arrOfString = new String[] { "a", "b", "c", "d", "e", "f", "g", "g"};
+		Stream<String> streamOfStringFromDeclaredArray = Arrays.stream(arrOfString);
+		// creation of Stream from collection
+		List<String> listOfString = Arrays.asList(arrOfString);
+		Stream<String> streamOfStringFromCollection = listOfString.stream();
+		
+		//Multi-threading with streams, where as printing order is not guaranteed
+		System.out.println("below is printed from parallel stream");
+		listOfString.parallelStream().forEach(a -> System.out.println(a));
+		
+		//getting count from stream
+		System.out.println("regular count: " + streamOfStringFromCollection.count());
+		//Stream is single use, here we can see that we'll get exception for IllegalStateException with "stream has already been operated upon or closed"
+		try {
+			System.out.println("distinct then count: " + streamOfStringFromCollection.distinct().count());
+		} catch (IllegalStateException ex) {
+			System.out.println("we caught IllegalStateException from the already used stream");
+		}
+		//to use the stream again, we can create the stream again using the list, this time we'll distinct it
+		System.out.println("distinct then count from the recreated stream: " + listOfString.stream().distinct().count());
+		
+		
+		//stream can be used to loop
+		System.out.println("below is printed from stream for each");
+		listOfString.stream().forEach(e -> System.out.println(e));
+		boolean foundLetterA = listOfString.stream().anyMatch(e -> e.contains("a"));
+		System.out.println("found letter a: " + foundLetterA);
+		
+		System.out.println("filtering the stream and creates a new stream, this case we are filtering g");
+		Stream<String> filteredStream = listOfString.stream().filter(e -> e.contains("g"));
+		filteredStream.parallel().forEach(e -> System.out.println(e));
+		
+		//converting Stream<String> to Stream<Path> using map from stream
+		List<String> uris = new ArrayList<>();
+		uris.add("C:\\My.txt");
+		Stream<Path> stream = uris.stream().map(uri -> Paths.get(uri));
+		stream.forEach(e -> System.out.println(e));
+		
+
+	}
+
+}
